b77917b72a9b320a903f41e6729044ef938e562c
Alissa Schlecht <schlecht220@gmail.com>
2017-02-24 11:03:27 +0000

    add skeleton

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// document.getElementsByClassName("product")[1].innerHTML = "aaaaaaqwerty";
+
+function showOptions(e){
+		e.innerHTML = "Not a button";
+		e.className += 'NewClass';
+}
+
+
+function addToCart() {
+
+}
+
+function increaseItemNum() {
+
+}
+
+function decreaseItemNum() {
+	
+}
+
+function deleteFromCart() {
+
+}
